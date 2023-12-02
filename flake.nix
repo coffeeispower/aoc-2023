be67ae066f0016b397aa220b3916d45d65c82e42
coffee-is-power <tiagodinis33@proton.me>
2023-12-02 12:30:47 +0000

    first commit: setup environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "roice development environment";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          ];
+        };
+      }
+    ); 
+}
